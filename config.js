de2e3a10aeca25a11d36aac22affeb2f555b6b9c
Yuvaraju Sai Kumar <157206085+yuvarajusaikumar@users.noreply.github.com>
2024-11-24 19:03:41 +0530

    Update and rename config.json to config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+
+module.exports = {
+    apiKey: '1234567890abcdef',  // Simulated API Key
+    secretKey: 's3cr3t_k3y',     // Simulated Secret Key
+    databasePassword: 'dbP@ssw0rd', // Simulated Database Password
+};
